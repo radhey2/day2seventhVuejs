eea32ff54e89c7f916b7425685c4dfb0e9f72b09
lenovo <radheshyamb9@gmail.com>
2022-06-09 11:05:15 +0530

    ADDED :Loops through a set of values(Numbers)

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+ <div id="app">
+  <ul>
+    <li v-for="num in 5" :key="num">
+      {{ num }}
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
